03b10b5b66956720bca2d4f0cefb76b8a77faa8d
enriquefnt <enriquefinetti@gmail.com>
2021-11-03 20:02:26 -0300

    css_w3 estilos

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -2,15 +2,16 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="../estilos/lay_style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<title><?=$title?></title>
 </head>
 <body>
-<header>
+<header class="w3-container w3-teal">
 <h1>Registros de datos</h1>
 <h3>Sectores descubiertos</h3>
 </header>
-<nav>
+<nav class="w3-color-green">
 <ul>
 <li><a href="../public/index.php">Inicio</a></li>
 <li><a href="../includes/buscacaso.php">Carga Datos</a></li>
@@ -25,7 +26,7 @@
 
 </main>
 
-<footer class="seccion-final">
+<footer class="w3-container w3-teal">
 <h3 class="seccion-final__parrafo"> DNyAS - Programa: Observatorio Vigilancia Nutricional</h3>
 
 </footer>
