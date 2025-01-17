176b654a77926c41d30aea84fcdfdf895f4d4ddf
enriquefnt <enriquefinetti@gmail.com>
2025-01-17 11:50:36 -0300

    cambio ip

--- includes/conect.php
+++ includes/conect.php
@@ -11,7 +11,7 @@
 <?php
 try {
 
-$pdo = new PDO('mysql:host=212.1.210.51;dbname=saltaped_relbas;charset=utf8', 'saltaped_descu', 'descu12#');
+$pdo = new PDO('mysql:host=109.106.251.18;dbname=saltaped_relbas;charset=utf8', 'saltaped_descu', 'descu12#');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
