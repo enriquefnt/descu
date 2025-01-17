ffc34776f66679dc1aa6ce328a16a5e5c599abf5
enriquefnt <enriquefinetti@gmail.com>
2025-01-17 12:15:12 -0300

    cambio ip

--- includes/conect.php
+++ includes/conect.php
@@ -1,12 +1,7 @@
-
-
 <?php
 //$pdo = new PDO('mysql:host=localhost;dbname=baserel;charset=utf8', 'enriquefnt', 'enfi7625');
 //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
-
-
-
 
 <?php
 try {
