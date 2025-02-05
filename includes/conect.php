45a93ee36d7e48201d585c49aad333c8ef9556b1
enriquefnt <enriquefinetti@gmail.com>
2025-02-05 13:09:38 -0300

    bajado de saltaped

--- includes/conect.php
+++ includes/conect.php
@@ -1,3 +1,23 @@
+
+
+
+
+<?php
+
+//$pdo = new PDO('mysql:host=localhost;dbname=baserel;charset=utf8', 'enriquefnt', 'enfi7625');
+
+//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
+
+
+
+
+
+
+
+
+
 <?php
 
 try {
